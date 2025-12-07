35f2e75a79d1c1002cba6a3af75aaf472633c696
Daniil Kucherenko <dania_kucherenko@outlook.com>
2025-12-07 21:42:08 +0100

    setup husky pre-commit hook

--- client/src/pages/platform/workflow-editor/components/WorkflowNodesPopoverMenuComponentList.tsx
+++ client/src/pages/platform/workflow-editor/components/WorkflowNodesPopoverMenuComponentList.tsx
@@ -98,7 +98,7 @@ const WorkflowNodesPopoverMenuComponentList = memo(
                         }),
                     });
                     return {definition: response, name};
-                } catch (error) {   
+                } catch (error) {
                     console.error(`[ERROR] Failed to fetch full component info for ${name}@v${version}:`, error);
                     return null;
                 } finally {
